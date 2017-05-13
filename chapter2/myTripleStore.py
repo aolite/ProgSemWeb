09cce0de7fa3c542c5e96c717b6fab6a8f6854bc
aolite <aitorcorchero@gmail.com>
2017-05-13 14:19:35 +0200

    chapter2 > myTripleStore > remove

--- chapter2/myTripleStore.py
+++ chapter2/myTripleStore.py
@@ -1,3 +1,4 @@
+
 class SimpleGraph:
     def __init__(self):
         self._spo = {}
@@ -17,4 +18,25 @@ class SimpleGraph:
                 index[a][b] = set([c])
             else:
                 index[a][b].add(c)
+
+    def remove(self, (sub, pred, obj)):
+
+        triples = list(self.triples((sub, pred, obj)))
+
+        for (delSub, delPred, delObj) in triples:
+            self._removeFromIndex(self._spo, delSub, delPred, delObj)
+            self._removeFromIndex(self._pos, delPred, delObj, delSub)
+            self._removeFromIndex(self._osp, delObj, delSub, delPred)
+
+    def _removeFromIndex(self, index, a, b, c):
+        try:
+            bs = index[a]
+            cset = bs[b]
+            cset.remove(c)
+            if len(cset) == 0: del bs[b]
+            if len(bs) == 0: del index[a]
+        # KeyErrors occur if a term was missing, which means that it wasn't a # valid delete:
+        except KeyError:
+            pass
+
 
